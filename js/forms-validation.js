12a1db35f0d7cf1e2991f950dd23db4d2b2ccf67
MatiasF <matiasferrer@live.com>
2019-12-15 20:14:12 -0300

    Completé la validación del formulario de Registro

--- js/forms-validation.js
+++ js/forms-validation.js
@@ -12,56 +12,117 @@ let fieldName = document.querySelector('#name');
 let fieldPass = document.querySelector('#pass');
 let fieldConfirmPass = document.querySelector('#confirm-pass');
 
-/* Button */
+/* Formats */
+const REGEX_MAIL = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+/* Buttons */
 let formButton = document.querySelector('.form-btn');
 
 /* Messages */
-let helperUserNameFormat = document.querySelector('.user-name-format-helper');
-let alertUserNameEmpty   = document.querySelector('.user-name-empty-alert');
-let alertUserNameLength  = document.querySelector('.user-name-length-alert');
+let helperUserNameFormat = document.querySelector('.username-format');
+let errorUserNameEmpty   = document.querySelector('.username-empty');
+let errorUserNameLength  = document.querySelector('.username-length');
 
-let alertNameEmpty = document.querySelector('.name-empty-alert');
+let errorEmailFormat = document.querySelector('.email-format');
+let errorEmailEmpty  = document.querySelector('.email-empty');
+
+let errorNameEmpty = document.querySelector('.name-empty');
+
+let helperPassFormat = document.querySelector('.pass-format');
+let errorPassEmpty  = document.querySelector('.pass-empty');
+let errorPassLength  = document.querySelector('.pass-length');
+
+let errorConfirmPassEmpty  = document.querySelector('.confirmpass-empty');
+let errorConfirmPassMatching = document.querySelector('.confirmpass-diff');
+
+let registerFormSent = document.querySelector('.successfully-registered');
 
 
+/* Validations */
+form.onsubmit = function(event) {
+    if(fieldUserName.value.trim() == '') {
+        errorUserNameEmpty.style.display = 'block';
+        event.preventDefault();
+    }
+    else if(fieldEmail.value.trim() == '') {
+        errorEmailEmpty.style.display = 'block';
+        event.preventDefault();
+    }
+    else if(fieldName.value.trim() == '') {
+        errorNameEmpty.style.display = 'block';
+        event.preventDefault();
+    }
+    else if(fieldPass.value.trim() == '') {
+        errorPassEmpty.style.display = 'block';
+        event.preventDefault();
+    }
+    else if(fieldConfirmPass.value.trim() == '') {
+        errorConfirmPassEmpty.style.display = 'block';
+        event.preventDefault();
+    }
+    else if(fieldPass.value != fieldConfirmPass.value) {
+        errorConfirmPassMatching.style.display = 'block';
+        event.preventDefault();
+    }
+    else {
+        errorUserNameEmpty.style.display = 'none';
+        errorEmailEmpty.style.display = 'none';
+        errorNameEmpty.style.display = 'none';
+        errorPassEmpty.style.display = 'none';
+        errorConfirmPassEmpty.style.display = 'none';
+        errorConfirmPassMatching.style.display = 'none';
+        registerFormSent.style.display = 'block';
+        event.preventDefault();
+    }
+};
 
 fieldUserName.onfocus = function() {
     helperUserNameFormat.style.display = 'block';
-    alertUserNameEmpty.style.display = 'none';
-    alertUserNameLength.style.display = 'none';
+    errorUserNameLength.style.display = 'none';
+    errorUserNameEmpty.style.display = 'none';
 };
-
 fieldUserName.onblur = function() {
-    if (this.value.trim() == '') {
+    if (this.value.length > 0 && this.value.length < 4) {
         helperUserNameFormat.style.display = 'none';
-        alertUserNameEmpty.style.display = 'block';
-        alertUserNameLength.style.display = 'none';
-    }
-    else if (this.value.length < 4) {
+        errorUserNameLength.style.display = 'block';
+    } else {
         helperUserNameFormat.style.display = 'none';
-        alertUserNameLength.style.display = 'block';
-    }
-    else {
-        helperUserNameFormat.style.display = 'none';
-        alertUserNameEmpty.style.display = 'none';
-        alertUserNameLength.style.display = 'none';
     }
 };
 
+fieldEmail.onfocus = function() {
+    errorEmailEmpty.style.display = 'none';
+}
+fieldEmail.onblur = function() {
+    if (this.value.length > 0 && !REGEX_MAIL.test(this.value)) {
+        errorEmailFormat.style.display = 'block';
+    } else {
+        errorEmailFormat.style.display = 'none';
+    }
+};
 
 fieldName.onfocus = function() {
-    alertNameEmpty.style.display = 'none';
-};
+    errorNameEmpty.style.display = 'none';
+}
 
-fieldName.onblur = function() {
-    if (this.value.trim() == '') {
-        alertNameEmpty.style.display = 'block';
+fieldPass.onfocus = function() {
+    helperPassFormat.style.display = 'block';
+    errorPassLength.style.display = 'none';
+    errorPassEmpty.style.display = 'none';
+    errorConfirmPassMatching.style.display = 'none';
+};
+fieldPass.onblur = function() {
+    if (this.value.length > 0 && this.value.length < 6) {
+        helperPassFormat.style.display = 'none';
+        errorPassLength.style.display = 'block';
+    } else {
+        helperPassFormat.style.display = 'none';
     }
-    else {
-        alertNameEmpty.style.display = 'none';
-    }
+};
+
+fieldConfirmPass.onfocus = function() {
+    errorConfirmPassEmpty.style.display = 'none';
+    errorConfirmPassMatching.style.display = 'none';
 };
 
 
-form.onsubmit = function(event) {
-    event.preventDefault();
-};

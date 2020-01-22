e888db2b711248503cce6feffd2a97f196f4d1e5
MatiasF <matiasferrer@live.com>
2020-01-22 02:09:11 -0300

    Mejoras de estilo, estrucutra y UX

--- js/main.js
+++ js/main.js
@@ -14,3 +14,33 @@ fetch("http://my-json-server.typicode.com/Matias-F/proyecto-transporte/servicios
 })
 
 
+
+/* Dinamyc Navbar
+********************/
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = () => {
+
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+
+  prevScrollpos = currentScrollPos;
+  
+}
+
+
+
+/* Stepper
+*************/
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  window.stepper = new Stepper(document.querySelector('#stepper'))
+  
+})
